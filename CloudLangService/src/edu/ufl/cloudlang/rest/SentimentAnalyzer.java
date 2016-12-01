66012979ea0f26dc4daca53af27c6b18fa50b192
Sayak Biswas <satak90@gmail.com>
2016-12-01 15:47:10 -0500

    Code cleanup

--- CloudLangService/src/edu/ufl/cloudlang/rest/SentimentAnalyzer.java
+++ CloudLangService/src/edu/ufl/cloudlang/rest/SentimentAnalyzer.java
@@ -3,7 +3,9 @@
  */
 package edu.ufl.cloudlang.rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -34,18 +36,31 @@ public class SentimentAnalyzer {
 	@GET
 	@Produces("application/json")
 	public Response parseTextWithInput(@PathParam("text") String text) throws JSONException {
-		String inputPath = "";
-		String outputPath = "";
-		String command = "";
+		String command = "THEANO_FLAGS=\"floatX=float32\" python lstm.py '" + text + "'";
+		Process process = null;
+		String log = null;
+		String sentimentResult = null;
+		BufferedReader stdInput = null;
 		try {
 			System.out.println("Will execute command " + command);
-			Runtime.getRuntime().exec(command);
+			process = Runtime.getRuntime().exec(command);
+			process.waitFor();
+			if(process != null) {
+				stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				System.out.println("Process logs :: ");
+				while ((log = stdInput.readLine()) != null) {
+					System.out.println(log);
+				}
+			}
 		} catch (IOException ie) {
 			ie.printStackTrace();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
 		}
-		
+		sentimentResult = log;
 		JSONObject json =  new JSONObject();
-		json.put("input text", text);
+		json.put("inputText", text);
+		json.put("sentimentResult", sentimentResult);
 		
 		return Response.status(200).entity(json.toString()).build();
 	}
